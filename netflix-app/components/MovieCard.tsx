f04c1e67157aa6fa9f04402eda482f9c8b18e3e3
ncls-brn <nicolas.j.baran@gmail.com>
2023-05-02 20:05:32 +0200

    movie card background fix problem with cursor

--- netflix-app/components/MovieCard.tsx
+++ netflix-app/components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
 
 interface MovieCardProps{
     data: Record<string,any>;
@@ -42,6 +43,37 @@ const MovieCard: React.FC<MovieCardProps> =({
             group-hover:-translate-y-[6vw]
             group-hover:translate-x-[2vw]
             group-hover:opacity-100">
+
+                <img className="
+                cursor-pointer
+                object-cover
+                transition
+                duration
+                shadow-xl
+                w-full
+                h-[12vw]" 
+                
+                src={data.thumbnailUrl} alt="Thumbnail"/>
+
+                <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
+                    <div className="flex flex-row items-center gap-3">
+                        <div className="
+                        cursor-pointer
+                        w-6
+                        h-6
+                        lg:w-10
+                        lg:h-10
+                        bg-white
+                        rounded-full
+                        flex
+                        justify-center
+                        items-center
+                        transition
+                        hover:bg-neutral-300" onClick={()=>{}}>
+                            <BsFillPlayFill size={30}/>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
