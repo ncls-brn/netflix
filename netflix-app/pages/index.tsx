55484681614a6ed658975cd2287bf6b01d71b79a
ncls-brn <nicolas.j.baran@gmail.com>
2023-05-15 20:12:07 +0200

    fix videos issues

--- netflix-app/pages/index.tsx
+++ netflix-app/pages/index.tsx
@@ -1,21 +1,22 @@
-import Billboard from "@/components/Billboard";
-import MovieList from "@/components/MovieList";
-import Navbar from "@/components/Navbar";
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
-import useMovieList from "@/hooks/useMovieList";
-import useFavorites from "@/hooks/useFavorites";
-import InfoModal from "@/components/InfoModal";
-import useInfoModalStore from "@/hooks/useInfoModalStore";
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
 
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import InfoModal from '@/components/InfoModal';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
 
-export async function getServerSideProps(context: NextPageContext){
-   const session = await getSession(context);
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-   if(!session){
-    return{
+  if (!session) {
+    return {
       redirect: {
-        destination:'/auth',
+        destination: '/auth',
         permanent: false,
       }
     }
@@ -26,22 +27,22 @@ export async function getServerSideProps(context: NextPageContext){
   }
 }
 
-export default function Home() {
-
-const {data: movies=[]} = useMovieList();
-const {data: favorites=[]}= useFavorites();
-const {isOpen, closeModal} = useInfoModalStore();
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModalStore();
 
   return (
     <>
-    <InfoModal visible={isOpen} onClose={closeModal}/>
-    <Navbar/>
-    <Billboard/>
-    <div className="pb-40">
-      <MovieList title="Trending Now" data={movies}/>
-      <MovieList title="My List" data={favorites}/>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
-    
     </>
   )
 }
+
+export default Home;
